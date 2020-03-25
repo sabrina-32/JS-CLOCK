e065787bc10844bdae915593e481c040aae6d462
Sabrina <sabrina32@student.sust.edu>
2020-03-25 20:02:42 +0600

    clock

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,31 @@
+const  secondHand = document.querySelector('.second-hand');
+const  minHand = document.querySelector('.min-hand');
+const  hourHand = document.querySelector('.hour-hand');
+
+
 function setData(){
 const  now = new Date();
+
+///hand  of second
+
 const  seconds =  now.getSeconds();
- console.log(seconds);
-    
+const  secondsDegrees = ((seconds/60)*360)+90;
+secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+
+/// hand  of  minutes
+const  mins = now.getMinutes();
+const minsDegree = ((mins/60)*360)+90;   
+minHand.style.transform = `rotate(${minsDegree}deg)`;
+
+///hand  of  hour
+
+const  hour = now.getMinutes();
+const hoursDegree = ((mins/12)*360)+90;   
+hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+
+
+
 }
 
 setInterval(setData,1000);
